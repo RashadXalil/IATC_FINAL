68dadb6ca4416f2d4cf31df76275039e28597bf4
RashadXalil <rashadmkh@code.edu.az>
2022-08-13 14:23:44 +0400

    slider commit

--- final-front/src/components/Main/Slider/Slider.jsx
+++ final-front/src/components/Main/Slider/Slider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Slider.css";
 const Slider = (props) => {
   const slider = props.slider;
+  console.log(slider);
   return (
     <div className="single-slider">
       <img src={slider.img} alt="" />
--- final-front/src/components/Main/SliderList/SliderList.jsx
+++ final-front/src/components/Main/SliderList/SliderList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
 import SingleSlider from '../Slider/Slider'
-import { useState, useEffect } from "react";
 const SliderList = (props) => {
   const sliders = props.sliders;
   return (
